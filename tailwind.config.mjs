1763cab3c519856df3f48492ac7921c6cf95d06f
Renzo Santos <orrego.st.santos@gmail.com>
2024-11-02 23:19:18 -0500

    add pastor page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: { mc1: "#012f5f", mc2: "#023060", mc3: "#0e96d1", mc4: "#99f7f8" },
+    extend: {},
+  },
+  plugins: [],
+};
