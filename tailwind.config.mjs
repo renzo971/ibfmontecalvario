379cb8f76cfdc67c92e00ee5852925d2f67a5388
Renzo Santos <orrego.st.santos@gmail.com>
2024-11-02 23:52:34 -0500

    add colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    colors: { mc1: "#012f5f", mc2: "#023060", mc3: "#0e96d1", mc4: "#99f7f8" },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      mc1: "#012f5f",
+      mc2: "#023060",
+      mc3: "#0e96d1",
+      mc4: "#99f7f8",
+    },
     extend: {},
   },
   plugins: [],
