b8f02878cc45d0254e014333c5e5d36e079399ef
Renzo Santos <orrego.st.santos@gmail.com>
2024-11-20 23:53:02 -0500

    add servicepage

--- src/components/Formcontact.tsx
+++ src/components/Formcontact.tsx
@@ -35,25 +35,63 @@ export default function FormContact() {
   }, [responseMessage]);
   return (
     <form onSubmit={submit}>
-      <label htmlFor="name">
-        Name
-        <input type="text" id="name" name="name" autoComplete="name" required />
-      </label>
-      <label htmlFor="email">
-        Email
+      <div className="mb-6">
+        <label
+          htmlFor="name"
+          className="block mb-2 text-sm font-medium text-gray-900 "
+        >
+          Nombre
+        </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          autoComplete="name"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+          placeholder="Nombre..."
+          required
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          htmlFor="Email"
+          className="block mb-2 text-sm font-medium text-gray-900 "
+        >
+          Email
+        </label>
         <input
           type="email"
           id="email"
           name="email"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
           autoComplete="email"
+          placeholder="Emal..."
           required
         />
-      </label>
-      <label htmlFor="message">
-        Message
-        <textarea id="message" name="message" autoComplete="off" required />
-      </label>
-      <button disabled={isLoading}>{isLoading ? "Enviando..." : "Send"}</button>
+      </div>
+      <div className="mb-6">
+        <label
+          htmlFor="message"
+          className="block mb-2 text-sm font-medium text-gray-900 "
+        >
+          Mensaje
+        </label>
+        <textarea
+          id="message"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+          name="message"
+          autoComplete="off"
+          placeholder="Dejanos un mensaje..."
+          required
+        />
+      </div>
+
+      <button
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        disabled={isLoading}
+      >
+        {isLoading ? "Enviando..." : "Enviar"}
+      </button>
       {responseMessage && <Toast />}
     </form>
   );
